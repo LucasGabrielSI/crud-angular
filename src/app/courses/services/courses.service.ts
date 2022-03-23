ee552f9106fe6dba49d0187628bce4d8240465ea
Lucas Gabriel de Oliveira Bento <lucasiguape16@gmail.com>
2022-03-23 12:03:15 -0300

    configure course service to return courses list.

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap,first } from 'rxjs/operators';
 
 import { Course } from '../model/course';
 
@@ -8,11 +9,15 @@ import { Course } from '../model/course';
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(private http: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'front-end'}
-    ];
+  list() {
+    return this.http.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      tap(courses => console.log(courses))
+    );
   }
 }

8ad810649a38312e41b71090c663329f2fbf249f
Lucas Gabriel de Oliveira Bento <lucasiguape16@gmail.com>
2022-03-24 16:25:53 -0300

    solving error treatment and creating popup for user visualizing error

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -8,7 +8,6 @@ import { Course } from '../model/course';
   providedIn: 'root'
 })
 export class CoursesService {
-
   private readonly API = '/assets/courses.json';
 
   constructor(private http: HttpClient) { }
@@ -17,7 +16,7 @@ export class CoursesService {
     return this.http.get<Course[]>(this.API)
     .pipe(
       first(),
-      delay(4000),
+      delay(5000),
       tap(courses => console.log(courses))
     );
   }
